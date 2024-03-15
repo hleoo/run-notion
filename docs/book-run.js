790bcdc8ca26d58f307b2fa72fac7b8d1ec8a097
Leo <48129608+hleoo@users.noreply.github.com>
2024-03-15 12:12:16 +0700

    Update book-run.js
    remove content type

--- docs/book-run.js
+++ docs/book-run.js
@@ -23,9 +23,6 @@
     fetch(webhookUrl, {
             method: 'POST',
             body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
         })
         .then(response => response.text())
         .then(result => {
